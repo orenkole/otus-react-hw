8fe9c932c1743f2628e1e636db17f2bde78ce428
badger <badger@badger.local>
2022-11-01 20:01:02 +0200

    try provider in stories

--- src/components/Cell/Cell.stories.tsx
+++ src/components/Cell/Cell.stories.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Story as StoryType } from "@storybook/react";
 import { Cell, CellPropsType } from ".";
+import { Provider } from "react-redux";
+import { setupStore } from "@/redux/store";
 
 const Story = {
   title: "Cell",
   component: Cell,
 };
 
-const Template: StoryType<CellPropsType> = (args) => <Cell {...args} />;
+const Template: StoryType<CellPropsType> = (args) => (
+  <Provider store={setupStore()}>
+    <Cell {...args} />
+  </Provider>
+);
 
 export const CellInitial = Template.bind({});
 CellInitial.args = {
--- src/components/SignInForm/SignInForm.stories.tsx
+++ src/components/SignInForm/SignInForm.stories.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Story as StoryType } from "@storybook/react";
 import { SignInForm, SignInFormPropsType } from ".";
 import StoryRouter from "storybook-react-router";
+import { Provider } from "react-redux";
+import { setupStore } from "@/redux/store";
 
 const Story = {
   title: "Sign in form",
   component: SignInForm,
 };
 
-const Template: StoryType<SignInFormPropsType> = (args) => <SignInForm {...args} />;
+const Template: StoryType<SignInFormPropsType> = (args) => (
+  <Provider store={setupStore()}>
+    <SignInForm {...args} />
+  </Provider>
+);
 
 export const Default = Template.bind({});
 Default.decorators = [StoryRouter];
